6ebc2403abaaa3cd453f0e70d385a7a79cf4d262
yuleehana <yuleehana@gmail.com>
2025-09-26 12:41:28 +0900

    yhn-팝업css추가0926

--- 02.map/js/ysl-sub-map-select-wrap.js
+++ 02.map/js/ysl-sub-map-select-wrap.js
@@ -18,7 +18,21 @@ selectBtn1.addEventListener("click",()=>{
     selWrap1.classList.toggle("active");
 })
 
+// map filter popup active
+let mapFilterBtn = document.querySelector(".filter a");
+let mapDim = document.querySelector(".map-filter-popup-wrap");
+let mapFilterCloseBtn = document.querySelector(".map-filter-popup-closeBtn>a");
+let mapFilterApply = document.querySelector(".map-filter-btn>.map-apply-btn");
 
+mapFilterBtn.addEventListener("click",()=>{
+    mapDim.classList.add("active");
+})
+mapFilterCloseBtn.addEventListener("click",()=>{
+    mapDim.classList.remove("active");
+})
+mapFilterApply.addEventListener("click", function(){
+    mapDim.classList.remove("active");
+})
 
 // map card img active
 // let mapCard = document.querySelectorAll(".map-card");
